82d85e228dc432b5edd59d4ee545bbb30facd0f6
emmanuel.anichebe <obumanichebe@gmail.com>
2021-02-18 16:00:19 +0100

    send emails on Thursdays using SMTP an datetime

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-import smtplib
+import smtplib  # Allows dev to send email messages using python
+import datetime as dt
+import random
+
+now = dt.datetime.now()
+week_day = now.weekday()  # gets week day as integer
+print(week_day)
+
+with open('quotes.txt') as quote_data:
+    quote_list = quote_data.readlines()
+
+random_quote = random.choice(quote_list)
+
+print(random_quote)
 
 my_email = "obumanichebe@gmail.com"
 password = 'stevengerad8'
 
-with smtplib.SMTP('smtp.gmail.com') as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email, to_addrs="bumski10.oa@gmail.com", msg='Hello')
-
+if week_day == 3:
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()  # secures connection with  the email sever.
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="bumski10.oa@gmail.com",
+                            msg=f'Subject:Weekly Motivational Quotes\n\n{random_quote}')
